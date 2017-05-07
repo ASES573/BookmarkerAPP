1097566d7b72f78d6943862b9671d4a113cae0f9
ganeshmkumar <ganeshkumar.m1996@gmail.com>
2017-05-07 17:33:17 +0530

    Changed the code to verify url pattern

--- js/main.js
+++ js/main.js
@@ -7,10 +7,6 @@ function saveBookmark(e){
   var siteName =document.getElementById('siteName').value;
   var siteUrl =document.getElementById('siteUrl').value;
 
-  if(!validateForm(siteName, siteUrl)){
-    return false;
-  }
-
   var bookmark = {
     name: siteName,
     url: siteUrl
@@ -89,22 +85,4 @@ function fetchBookmarks(){
                                   '</h3>'+
                                   '</div>';
   }
-}
-
-// Validate Form
-function validateForm(siteName, siteUrl){
-  if(!siteName || !siteUrl){
-    alert('Please fill in the form');
-    return false;
-  }
-
-  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-  var regex = new RegExp(expression);
-
-  if(!siteUrl.match(regex)){
-    alert('Please use a valid URL');
-    return false;
-  }
-
-  return true;
 }
